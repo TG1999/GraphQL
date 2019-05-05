90bbfc92c136cfd3f875cb26f5d4d5da4fd6ff22
TG1999 <tushar.goel.dav@gmail.com>
2019-05-05 20:45:53 +0530

    mutation added
    Signed-off-by: TG1999 <tushar.goel.dav@gmail.com>

--- schema.js
+++ schema.js
@@ -5,8 +5,24 @@ type Obj{
     name:String
     time:String
 }
+type User{
+    firstName:String!
+    lastName:String!
+    emails:String
+    id:ID
+}
+input UserInput{
+    id:ID
+    firstName:String!
+    lastName:String!
+    emails:String
+}
 type Query{
     item : Obj
+    user:User
+}
+type Mutation{
+    createUser(input:UserInput): User
 }
 `)
 module.exports={
--- server.js
+++ server.js
@@ -5,12 +5,29 @@ const schema=require('./schema').schema
 app.get('/',(req,res)=>{
     res.send('Route is working fine')
 })
+users=[];
 root={
     item:()=>{
         return{
         name:'ABC',
         time:'12:32:11'
         }   
+    },
+    user:()=>{
+        return{
+            firstName:'Tushar',
+            lastName:'Goel',
+            emails:[{
+                email:'tush@goel.com'
+            },
+            {
+                email:'tush@goel2.com'
+            }]
+        }
+    },
+    createUser:({input})=>{
+        users.push(input)
+        return input
     }
 }
 app.use('/graphql',graphqlHTTP({
